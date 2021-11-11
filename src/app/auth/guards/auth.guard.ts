d01010fea5bed8ef97ed38d7bc8f6b5cc99c7b75
albmg <albmguer@gmail.com>
2021-11-11 11:29:14 +0000

    fin proteccion rutas

---
+++ src/app/auth/guards/auth.guard.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanLoad {
+
+  constructor( private authService: AuthService,
+               private router: Router ) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+
+      return this.authService.verificaAutenticacion()
+              .pipe(
+                tap( estaAutenticado => {
+                  if( !estaAutenticado ) {
+                    this.router.navigate(['./auth/login'])
+                  }
+                })
+              )
+
+  }
+
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+
+      return this.authService.verificaAutenticacion()
+              .pipe(
+                tap( estaAutenticado => {
+                  if( !estaAutenticado ) {
+                    this.router.navigate(['./auth/login'])
+                  }
+                })
+              )
+
+    return false;
+  }
+}
